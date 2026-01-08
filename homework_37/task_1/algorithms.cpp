9e37561ca1940b19f3e57620cfb26c3b33536ecb
root <root@Rub-ThinkBook.localdomain>
2026-01-08 20:27:44 +0400

    homework 37, task 1 done

--- homework_37/task_1/algorithms.cpp
+++ homework_37/task_1/algorithms.cpp
@@ -69,13 +69,14 @@ void	merge_sort(T *arr, std::size_t length)
 	if (length <= 1)
 		return ;
 
+	r_ind = 0;
 	mid	= length / 2;
 	left = new T[mid];
 	right = new T[length - mid];
 
 	for (std::size_t i = 0; i < mid; ++i)
 		left[i] = arr[i];
-	r_ind = 0;
+
 	for (std::size_t j = mid; j < length; ++j)
 		right[r_ind++] = arr[j];
 
@@ -88,38 +89,66 @@ void	merge_sort(T *arr, std::size_t length)
 }
 
 template <typename T>
-int	partition()
+std::size_t	partition(T *arr, std::size_t length)
 {
+	std::size_t	j;
+	std::size_t	ind;
+	T			pivot;
 
+	ind = length - 1;
+	pivot = arr[ind];
+	j = 0;
+
+	for (std::size_t i = 0; i < ind; ++i)
+	{
+		if (arr[i] < pivot)
+		{
+			swap(&arr[i], &arr[j]);
+			++j;
+		}
+	}
+	swap(&arr[j], &arr[ind]);
+	return (j);
 }
 
 template <typename T>
-void	quick_sort()
+void	quick_sort(T *arr, std::size_t length)
 {
+	std::size_t	pivot;
 
+	if (length <= 1)
+		return ;
+
+	pivot = partition(arr, length);
+	quick_sort(arr, pivot);
+	quick_sort(arr + pivot + 1, length - pivot - 1);
 }
 
 int	main()
 {
 	/*int arr*/
-	/*
-		int	arr[] = {5, 2, 9, 1, 5, 6, 9};
-		std::size_t	size = sizeof(arr) / sizeof(arr[0]);
-		merge_sort(arr, size);
-		for (std::size_t i = 0; i < size; ++i)
-			std::cout << arr[i] << " ";
-		std::cout << std::endl;
-	*/
+	int	arr1[] = {5, 2, 7, 1, 5, 6, 9};
+	std::size_t	size1 = sizeof(arr1) / sizeof(arr1[0]);
+	merge_sort(arr1, size1);
+	for (std::size_t i = 0; i < size1; ++i)
+		std::cout << arr1[i] << " ";
+	std::cout << std::endl;
 
 	/*double arr*/
-	/*
-		double	arr[] = {5.5, 2.2, 9.9, 1.1, 5.5, 6.6};
-		std::size_t	size = sizeof(arr) / sizeof(arr[0]);
-		bubble_sort(arr, size);
-		for (std::size_t i = 0; i < size; ++i)
-			std::cout << arr[i] << " ";
-		std::cout << std::endl;
-	*/
+	double	arr2[] = {5.5, 2.2, 9.9, 1.1, 5.5, 6.6};
+	std::size_t	size2 = sizeof(arr2) / sizeof(arr2[0]);
+	bubble_sort(arr2, size2);
+	for (std::size_t i = 0; i < size2; ++i)
+		std::cout << arr2[i] << " ";
+	std::cout << std::endl;
+
+	/*double arr*/
+	double	arr3[] = {5.5, 2.2, 9.9, 1.1, 5.5, 6.6};
+	std::size_t	size3 = sizeof(arr3) / sizeof(arr3[0]);
+	quick_sort(arr3, size3);
+	for (std::size_t i = 0; i < size3; ++i)
+		std::cout << arr3[i] << " ";
+	std::cout << std::endl;
 
 	return 0;
 }
