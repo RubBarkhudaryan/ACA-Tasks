bea4be2b95c2987cb597c996a55724cb1e671154
root <root@Rub-ThinkBook.localdomain>
2026-01-18 14:46:48 +0400

    homework 40

---
+++ homework_40/animal.h
@@ -0,0 +1,40 @@
+#ifndef ANIMAL_H
+
+# define ANIMAL_H
+
+# include <string>
+
+class	Animal
+{
+	protected:
+		std::string	_name;
+		std::string	_breed;
+		std::string	_color;
+		int			_age;
+		double		_weight;
+		double		_height;
+	public:
+
+		/*Animal ctor/dtor*/
+		Animal();
+		Animal(const Animal &other);
+		Animal(Animal &&other) noexcept;
+		Animal(
+			const std::string &name, const std::string &breed,
+			const std::string &color, int age,
+			double weight, double height
+		);
+		~Animal();
+
+		/*Animal operators*/
+		Animal			&operator=(const Animal &other);
+		Animal			&operator=(Animal &&other) noexcept;
+		bool			operator==(const Animal& other) const;
+
+		/*Animal methods*/
+		void			sound(std::string _sound) const;
+		int				age() const;
+		std::string		name() const;
+};
+
+#endif
---
+++ homework_40/main.cpp
@@ -0,0 +1,17 @@
+#include "./animal.h"
+#include "./animal_compare.h"
+#include <unordered_set>
+#include <iostream>
+
+int main()
+{
+	std::unordered_set<Animal, Hasher, AnimalCompare> animals;
+
+	animals.insert(Animal("Dog", "Rottweiler", "Black", 5, 20, 150.5));
+	animals.insert(Animal("Cat", "Siamese", "White", 3, 10, 50.0));
+	animals.insert(Animal("Horse", "Arabian", "Brown", 10, 30, 300.0));
+	animals.insert(Animal("Bird", "Parrot", "Green", 3, 5, 1.5));
+
+	for (const Animal& a : animals)
+		std::cout << a.name() << " - " << a.age() << std::endl;
+}
