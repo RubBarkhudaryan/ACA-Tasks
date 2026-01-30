72bd30bb6ad6689d2bdfddcedb41168d2cee5494
RubBarkhudaryan <rubenbarkhudaryan2005@gmail.com>
2026-01-30 23:04:09 +0400

    another solution given -> homework_44

--- homework_44/main2.cpp
+++ homework_44/main2.cpp
@@ -13,3 +13,25 @@ int main()
 {
 	std::cout << closure(5)(6) << std::endl;
 }
+
+/*
+std::function<std::function<int (int)>()> closure = []() -> std::function<int (int)>
+{
+	int	prod = 1;
+
+	return [prod](int num) mutable
+	{
+		prod *= num;
+		return (prod);
+	};
+};
+
+int main()
+{
+	std::function<int (int)> f = closure();
+	std::cout << f(5) << std::endl;
+	std::cout << f(6) << std::endl;
+}
+
+// this alternative was built before.
+*/
